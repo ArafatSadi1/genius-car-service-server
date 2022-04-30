db88db4c8033dbc88a8dc13e616c0af0de9d82bc
ArafatSadi1 <arafatsadi1@gmail.com>
2022-04-30 11:35:03 +0600

    secure api with jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,20 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'Unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2r1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,7 +32,18 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('genius-car').collection('service');
+        const orderCollection = client.db('genius-car').collection('orders');
 
+        // Auth
+        app.post('/login', async(req, res)=>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken})
+        })
+
+        // service api
         app.get('/service', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -38,6 +64,29 @@ async function run(){
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         })
+
+        // add order to db
+        app.post('/order', async(req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+
+        // get order collection from db
+        app.get('/order',verifyJWT, async(req, res)=>{
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if(decodedEmail === email){
+                const query = {email: email};
+                const filter = orderCollection.find(query);
+                const orders = await filter.toArray();
+                res.send(orders)    
+            }
+            else{
+                return res.status(403).send({message: 'Forbidden Access'})
+            }
+        })
+
         // delete
         app.delete('/service/:id', async(req, res)=>{
             const id = req.params.id;
@@ -54,7 +103,7 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res)=>{
-    res.send('server is running')
+    res.send('Genius car service server is running')
 })
 
 app.listen(port, ()=>{
